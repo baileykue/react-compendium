c7d33f2a2fcb507c4d1114c81681b8ac3ca9fac9
Bailey Kuebelbeck <bailey.kue@gmail.com>
2021-12-09 12:17:58 -0800

    starting to re factor and set up type filter through pokemon api in pokemon js

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import Controls from './components/Controls/Controls';
 import PokeList from './components/PokeList/PokeList';
-import { getPokemon } from './services/pokemon';
+import { getPokemon, getTypes } from './services/pokemon';
 
 function App() {
   const [pokemon, setPokemon] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+
   const [query, setQuery] = useState('');
   const [order, setOrder] = useState('asc');
-  const [type, setType] = useState('all');
+  const [types, setTypes] = useState([]);
+  const [selectType, setSelecType] = useState('all');
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = await getPokemon(query, order, type);
+      const data = await getPokemon(query, order);
       setPokemon(data.results);
       setLoading(false);
     };
@@ -21,7 +24,15 @@ function App() {
     if (loading) {
       fetchData();
     }
-  }, [loading, query, order, type]);
+  }, [loading, query, order]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getTypes();
+      setTypes(data);
+    };
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -10,44 +10,51 @@ export default function Controls({ query, setQuery, setLoading, type, setType, o
           setQuery(e.target.value);
         }}
       />
+
+      <label>
+        Sort:
+        <select
+          value={order}
+          onChange={(e) => {
+            setOrder(e.target.value);
+          }}
+        >
+          <option value="asc">Ascedning</option>
+          <option value="dsc">Descending</option>
+        </select>
+      </label>
+
+      <label>
+        Type:
+        <select
+          value={type}
+          onChange={(e) => {
+            setType(e.target.value);
+          }}
+        >
+          <option value="all">All</option>
+          <option value="bug">Bug</option>
+          <option value="electric">Electric</option>
+          <option value="fire">Fire</option>
+          <option value="grass">Grass</option>
+          <option value="normal">Normal</option>
+          <option value="rock">Rock</option>
+          <option value="dark">Dark</option>
+          <option value="fairy">Fairy</option>
+          <option value="flying">Flying</option>
+          <option value="ground">Ground</option>
+          <option value="poison">Poison</option>
+          <option value="steel">Steel</option>
+          <option value="dragon">Dragon</option>
+          <option value="fighting">Fighting</option>
+          <option value="ghost">Ghost</option>
+          <option value="ice">Ice</option>
+          <option value="psychic">Psychic</option>
+          <option value="water">Water</option>
+        </select>
+      </label>
+
       <button onClick={() => setLoading(true)}>Search</button>
-
-      <select
-        value={order}
-        onChange={(e) => {
-          setOrder(e.target.value);
-        }}
-      >
-        <option value="asc">Ascedning</option>
-        <option value="dsc">Descending</option>
-      </select>
-
-      <select
-        value={type}
-        onChange={(e) => {
-          setType(e.target.value);
-        }}
-      >
-        <option value="all">All</option>
-        <option value="bug">Bug</option>
-        <option value="electric">Electric</option>
-        <option value="fire">Fire</option>
-        <option value="grass">Grass</option>
-        <option value="normal">Normal</option>
-        <option value="rock">Rock</option>
-        <option value="dark">Dark</option>
-        <option value="fairy">Fairy</option>
-        <option value="flying">Flying</option>
-        <option value="ground">Ground</option>
-        <option value="poison">Poison</option>
-        <option value="steel">Steel</option>
-        <option value="dragon">Dragon</option>
-        <option value="fighting">Fighting</option>
-        <option value="ghost">Ghost</option>
-        <option value="ice">Ice</option>
-        <option value="psychic">Psychic</option>
-        <option value="water">Water</option>
-      </select>
     </div>
   );
 }
